66a85217e1d01a5528ea5faa1f04df7eeef132ff
Steve Grunwell <stevegrunwell@gmail.com>
2016-12-23 00:29:33 -0500

    Add the `sort_query_by_series_order()` function, which will sort $wp_query->posts by an arbitary $order array.

--- tests/PHPUnit/CoreTest.php
+++ tests/PHPUnit/CoreTest.php
@@ -31,4 +31,62 @@ class CoreTest extends \Growella\Anthology\TestCase {
 
 		enqueue_admin_assets();
 	}
+
+	public function testSortQueryBySeriesOrder() {
+		$posts = array();
+
+		for ( $i = 1; $i <= 3; $i++ ) {
+			$post = new \stdClass;
+			$post->ID = $i;
+
+			$posts[ $i ] = $post;
+		}
+
+		// Create a dummy WP_Query object; it doesn't need any methods.
+		$query = new \stdClass;
+		$query->posts = $posts;
+
+		$result = sort_query_by_series_order( $query, array( 2, 1, 3 ) );
+
+		$this->assertEquals( 0, array_search( $posts[2], $result->posts, true ) );
+		$this->assertEquals( 1, array_search( $posts[1], $result->posts, true ) );
+		$this->assertEquals( 2, array_search( $posts[3], $result->posts, true ) );
+	}
+
+	public function testSortQueryBySeriesOrderReturnsEarlyIfOrderIsEmpty() {
+		$query = new \stdClass;
+		$query->posts = array( 'foo' );
+
+		$this->assertEquals( $query, sort_query_by_series_order( $query, array() ) );
+	}
+
+	public function testSortQueryBySeriesOrderReturnsEarlyIfThereAreNoPosts() {
+		$query = new \stdClass;
+		$query->posts = array();
+
+		$this->assertEquals( $query, sort_query_by_series_order( $query, array( 2, 1, 3 ) ) );
+	}
+
+	public function testSortQueryBySeriesOrderPutsUnaccountedValuesAtEnd() {
+		$posts = array();
+
+		for ( $i = 1; $i <= 5; $i++ ) {
+			$post = new \stdClass;
+			$post->ID = $i;
+
+			$posts[ $i ] = $post;
+		}
+
+		// Create a dummy WP_Query object; it doesn't need any methods.
+		$query = new \stdClass;
+		$query->posts = $posts;
+
+		$result = sort_query_by_series_order( $query, array( 2, 1, 3 ) );
+
+		$this->assertEquals( 0, array_search( $posts[2], $result->posts, true ) );
+		$this->assertEquals( 1, array_search( $posts[1], $result->posts, true ) );
+		$this->assertEquals( 2, array_search( $posts[3], $result->posts, true ) );
+		$this->assertEquals( 3, array_search( $posts[4], $result->posts, true ) );
+		$this->assertEquals( 4, array_search( $posts[5], $result->posts, true ) );
+	}
 }
